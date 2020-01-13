5b4525f426b666755529085bf70799be2a797b7a
Andrei Niciu <andrei.niciu@gmail.com>
2020-01-13 13:21:06 +0200

    Create OptimizeBoxDimensions.py

---
+++ OptimizeBoxDimensions/OptimizeBoxDimensions.py
@@ -0,0 +1,5 @@
+# This python script is aiming at solving the problem mentioned at the link below (optimizing box dimensions to match given required volume) using the methods described at the link
+# https://towardsdatascience.com/optimization-with-scipy-and-application-ideas-to-machine-learning-81d39c7938b8
+
+import numpy as np
+import scipy.optimize as optimize
